43c382f1254c12429fe4107bc2a9a973efe9ee17
FinhasYohannesGustavo <pj.gustavojhon@gmail.com>
2024-03-11 08:40:20 +0300

    Modified the Product controller to use parameters as well as model binding to send data from views to controllers

--- Web II Labs/Controllers/ProductController.cs
+++ Web II Labs/Controllers/ProductController.cs
@@ -10,7 +10,8 @@ namespace Web_II_Labs.Controllers
             return View();
         }
 
-        [HttpGet] public IActionResult Create()
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
@@ -19,14 +20,35 @@ namespace Web_II_Labs.Controllers
         [ActionName("Create")]
         public IActionResult CreateProd()
         {
+
             ViewBag.Title = "From Form Request Dict";
             Product dummyProd = new Product();
             dummyProd.Id = int.Parse(Request.Form["ID"]);
-            dummyProd.Name= Request.Form["Name"];
+            dummyProd.Name = Request.Form["Name"];
             dummyProd.Description = Request.Form["Description"];
 
-            return View("CreateProduct",dummyProd);
+            return View("CreateProduct", dummyProd);
         }
+
+        //public IActionResult Edit(int id, String name, string Description)
+        //{
+        //    ViewBag.Title = "Recieved Data With Parametes";
+        //    Product product = new Product();
+        //    product.Id = id;
+        //    product.Name = name;
+        //    product.Description = Description;
+        //    return View("CreateProduct2", product);
+
+        //}
+
+        //Sending form data with model binded object
+        //public IActionResult CreateProd(Product product)
+        //{
+        //    ViewBag.Title = "Recieved data with model binded object";
+
+        //    return View("CreateProduct", product);
+
+        //}
 
     }
 }
